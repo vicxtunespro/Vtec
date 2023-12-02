8747674a5eb52fbf603846a19816edb059e71cf6
vicxtunespro <vicitug@gmail.com>
2023-12-02 18:42:01 +0000

    Made some changes on the footer.

--- app/layout.js
+++ app/layout.js
@@ -50,8 +50,13 @@ export default function RootLayout({ children }) {
               <Link href="/contact" className='page-link'>Contact us</Link>
             </nav>
           </div>
-        </div>  
+        </div>
         {children}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#172554" fill-opacity="1" d="M0,224L60,202.7C120,181,240,139,360,117.3C480,96,600,96,720,122.7C840,149,960,203,1080,213.3C1200,224,1320,192,1380,176L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+        <footer className='h-80 w-full'>
+
+        </footer>  
+        
       </body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import CardGroup from './components/cardGroup'
 
 export default function Home() {
   return (
-    <main className="min-h-screen px-8 md:px-16 lg:32">
-      <CardGroup/>
-    </main>
+      <main className="min-h-screen px-8 md:px-16 lg:32">
+        <CardGroup/>
+      </main>
   )
 }
