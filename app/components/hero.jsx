7ce7d0b47a66ef36c91b1ea6263fe222b616cceb
vicxtunespro <vicitug@gmail.com>
2023-12-09 14:37:27 +0000

    Added hero section.

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -1,15 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='w-screen hero'>
-        <div className='overlay'>
-            <h3 className='headline'>Your success partner </h3>
-            <p className='hero-content'>Get all technical service for your dream house. Cheap and good quality work</p>
-            <div className="action-btn pt-3 pr-3">
-                <button className='action1'>View all service</button>
-                <button className='action2'>Get in touch</button>
-            </div>
+    <div className='w-screen hero flex flex-wrap  p-2  lg:pl-40'>
+        <div class="flex flex-col justify-center items-center gap-2 text-center w-screen mt-10 md:mt-0 sm:w-1/2">
+          <p class="text-white text-2xl font-black lg:text-4xl ">Lets build the future together!</p>
+          <p class="text-white text-sm">Hurry and join our community now!</p>
+          <Link href="/" class="py-2 px-3 w-40 text-center text-white bg-orange-300 rounded">Join now</Link>
+        </div>
+        <div class="flex flex-col justify-center relative h-fill w-fill sm:w-1/2">
+          <Image src="/hero1.png" width={600} height={600} alt="hero photo">
+          </Image>
         </div>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import Card from './components/card'
 import CardGroup from './components/cardGroup'
+import Hero from './components/hero'
 
 export default function Home() {
   return (
-      <main className="min-h-screen px-8 md:px-16 lg:32">
-        <CardGroup/>
-      </main>
+      <div class="wrapper">
+        <Hero/>
+        <main className="min-h-screen px-8 md:px-16 lg:32">
+          <CardGroup/>
+        </main>
+      </div>
   )
 }
