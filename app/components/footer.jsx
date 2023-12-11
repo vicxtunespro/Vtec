9dffad5967d9232d13f5d17196ed80c5060dd50c
vicxtunespro <vicitug@gmail.com>
2023-12-11 15:36:29 +0000

    Editing Footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,9 +1,63 @@
+import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
+import { AiFillFacebook, AiFillLinkedin, AiFillInstagram, AiOutlineMenu  } from 'react-icons/ai';
 
-export default function footer() {
+export default function Footer() {
   return (
-    <footer>
-        
-    </footer>
+    <footer className='w-screen flex flex-col gap-2 pl-16'>
+      
+      <div className="quick-info flex gap-4">
+      <div className='text-white'>
+          <h1 className='text-xl pb-2 font-semibold'>Company</h1>
+          <div className='flex flex-col gap-3 font-light'>
+            <Link href='/'>Home</Link>
+            <Link href='/about'>About us</Link>
+            <Link href='/services'>E-learning</Link>
+            <Link href='/ebook'>E-books</Link>
+            <Link href='/contact'>Contact us</Link>
+            <Link href='/gallery'>Gallery</Link>
+        </div>
+        </div>
+        <div className='text-white'>
+          <h1 className='text-xl pb-2 font-semibold'>Services</h1>
+          <ul className='flex flex-wrap flex-col gap-2 font-light'>
+            <li>Custom Software Development</li>
+            <li>Web Application Development</li>
+            <li>Mobile App Development</li>
+            <li>Cloud Computing Solutions</li>
+            <li>IT Consulting Services</li>
+            <li>Cybersecurity Solutions</li>
+            <li>Database Management</li>
+            <li>Network Design and Implementation</li>
+            <li>IT Support and Maintenance</li>
+            <li>Digital Transformation Services</li>
+        </ul>
+        </div>
+        <div className='text-white'>
+          <h1 className='text-xl pb-2 font-semibold'>Customers</h1>
+          <ul className='flex flex-wrap flex-col gap-2 font-light'>
+            <li>Custom Software Development</li>
+            <li>Web Application Development</li>
+            <li>Mobile App Development</li>
+            <li>Cloud Computing Solutions</li>
+            <li>IT Consulting Services</li>
+            <li>Cybersecurity Solutions</li>
+            <li>Database Management</li>
+            <li>Network Design and Implementation</li>
+            <li>IT Support and Maintenance</li>
+            <li>Digital Transformation Services</li>
+        </ul>
+        </div>
+      </div>
+      <div class="icons flex w-screen justify-center mb-3">
+        <AiFillFacebook className='icon'/>
+        <AiFillInstagram className='icon'/>
+        <AiFillLinkedin className='icon'/>
+      </div>
+      <div class="icons flex w-screen justify-center mb-3">
+        <Image src='/logo.png' width={100} height={50}></Image>
+      </div>
+    </footer> 
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import { AiFillFacebook, AiFillLinkedin, AiFillInstagram, AiOutlineMenu  } from 
 import Link from 'next/link';
 import Navbar from './components/nav';
 import Image from 'next/image';
+import Footer from './components/footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -56,10 +57,7 @@ export default function RootLayout({ children }) {
         </div>
         {children}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319"><path fill="#00344A" fill-opacity="1" d="M0,224L60,202.7C120,181,240,139,360,117.3C480,96,600,96,720,122.7C840,149,960,203,1080,213.3C1200,224,1320,192,1380,176L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-        <footer className='h-80 w-full'>
-
-        </footer>  
-        
+        <Footer/> 
       </body>
     </html>
   )
