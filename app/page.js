879d35a9572e6dde1342019f78b29c386e8fda28
vicxtunespro <vicitug@gmail.com>
2023-12-19 14:02:30 +0000

    Swiper js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Card from './components/card'
 import CardGroup from './components/cardGroup'
 import Hero from './components/hero'
-import Slider from './components/swiper'
 
 
 
@@ -12,7 +11,6 @@ export default function Home() {
         <Hero/>
         <main className="min-h-screen px-8 md:px-16 lg:32">
           <CardGroup/>
-          <Slider></Slider>
         </main>
       </div>
   )
