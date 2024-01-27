7e4fb86e3f6db7e59e82fa740685cc909f16f44f
vicxtunespro <vicitug@gmail.com>
2024-01-27 23:06:30 +0000

    Shadows on mibile

--- app/components/caption.jsx
+++ app/components/caption.jsx
@@ -1,16 +1,18 @@
 
 import Image from 'next/image'
 import React from 'react'
+import { FaArrowRight, FaWhatsapp } from 'react-icons/fa'
 
 export default function ContentCard() {
   return (
-      <div className="w-full h-96 mt-3 mb-3 flex flex-col flex-basis-1/2 md:flex-row rounded shadow overflow-hidden">
-        <div className="h-1/2 w-full lg:w-1/2 lg:h-full bg-orange-400 relative">
+      <div className="w-full h-1/2 mt-3 mb-3 flex flex-col md:flex-row rounded shadow overflow-hidden">
+        <div className="h-72 w-full lg:w-1/2 lg:h-full bg-orange-400 relative">
             <Image src="/meetup.jpg" alt="caption photo" fill objectFit='cover'></Image>
 
         </div>
-        <div className='bg-blue-100 text-white dark:text-white w-full lg:w-1/2 p-10 flex items-center justify-center'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi a tenetur deserunt mollitia ducimus, molestiae delectus. Quas atque architecto exercitationem dolor nesciunt molestiae dolores nam. Itaque, quam. Nemo, inventore eos.
+        <div className='bg-blue-10 text-black dark:text-white w-full lg:w-1/2 p-10 flex flex-col gap-4'>
+            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi a tenetur deserunt mollitia ducimus, molestiae delectus. Quas atque architecto exercitationem dolor nesciunt molestiae dolores nam. Itaque, quam. Nemo, inventore eos.</p>
+            <button class="bg-blue-950 py-4 px-4 flex gap-2 justify-center items-center text-white">Join our Whatsapp group <FaWhatsapp/></button>
         </div>
       </div>
   )
