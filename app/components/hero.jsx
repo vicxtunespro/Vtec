61a822674870936eb8854ea89b995f61f89467a2
vicxtunespro <124519343+vicxtunespro@users.noreply.github.com>
2024-03-16 13:38:27 +0300

    Update hero.jsx

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className='w-screen h-1/2 bg-blue-100 pl-8 pr-8 pb-10 pt-24 flex flex-col justify-center items-center gap-8'>
         <h1 className='text-3xl font-bold text-center'>Grow your skill <br></br>with us</h1>
-        <p class="text-center">Every youth out there has the ability to set an opportunity</p>
+        <p class="text-center">Every youth out there got the ability to set an opportunity</p>
         <button className='py-3 x-5 w-32 bg-blue-400 text-white dark:text-white rounded'>Join now</button>
     </div>
   )
